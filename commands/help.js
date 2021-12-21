ecb89f22b4520c8cfe62dd85a820abf6b55fae72
cojored <heppconrad2@gmail.com>
2021-12-21 17:46:38 +0000

    helpcmd

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports = {
-  name: "socials",
-  description: "Socials",
+  name: "Help",
+  description: "Help Command",
   execute(message, args) {
     let embed = new Discord.MessageEmbed();
     embed.setTitle("Help");
@@ -12,7 +12,7 @@ module.exports = {
       db.set("embedColor", "#FF0000");
     }
     for (command in message.client.commands) {
-      embed.addField(command.name, command.description);
+      embed.addField("!" + command.name, command.description);
     }
     message.channel.send({ embeds: [embed] });
   },
