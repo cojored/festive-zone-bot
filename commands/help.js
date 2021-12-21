8360cf8d6000fb8d50301fa2873a355ec5c5297e
cojored <heppconrad2@gmail.com>
2021-12-21 17:46:26 +0000

    helpcmd

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "socials",
+  description: "Socials",
+  execute(message, args) {
+    let embed = new Discord.MessageEmbed();
+    embed.setTitle("Help");
+    embed.setColor(db.get("embedColor"));
+    if (db.get("embedColor") === "#FF0000") {
+      db.set("embedColor", "#00FF00");
+    } else {
+      db.set("embedColor", "#FF0000");
+    }
+    for (command in message.client.commands) {
+      embed.addField(command.name, command.description);
+    }
+    message.channel.send({ embeds: [embed] });
+  },
+};
